c464649f4d695d694e66ea685ad6f969b69f866f
Luis Valgoi <luisvalgoi@gmail.com>
2021-07-12 11:00:32 -0300

    chore: removed perge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
